42a5366bc34c804d920dfd67a0305f5dba5ada33
Matteo Barzaghi <153495525+matteobarzaghi@users.noreply.github.com>
2025-09-16 21:59:59 +0200

    Refactoring of Actions class

--- src/test/java/stepdefinitions/MyStepdefs.java
+++ src/test/java/stepdefinitions/MyStepdefs.java
@@ -1,7 +1,6 @@
 package stepdefinitions;
 
-import actions.LoginActions;
-import io.cucumber.java.After;
+import actions.Actions;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -10,30 +9,25 @@ import io.cucumber.java.en.When;
 import static org.junit.Assert.assertTrue;
 
 public class MyStepdefs {
-    private final LoginActions loginActions = new LoginActions();
+    private final Actions actions = new Actions();
 
     @Given("I am on the login page")
     public void iAmOnTheLoginPage() {
-        loginActions.landing();
+        //TODO
     }
 
     @When("I login as a {string}")
     public void iLoginAsA(String role) {
-        loginActions.login(role);
+        actions.login(role);
     }
 
     @Then("I should see the homepage")
     public void iShouldSeeTheHomepage() {
-        assertTrue("Text not found!", loginActions.checkHomepage().contains("Swag Labs"));
-    }
-
-    @After
-    public void tearDown() {
-        loginActions.closeBrowser();
+        assertTrue("Text not found!", actions.checkHomepage().contains("Swag Labs"));
     }
 
     @And("I see the label {string}")
     public void iSeeTheLabel(String label) {
-        assertTrue("Text not found!", loginActions.checkLabel().contains(label));
+        assertTrue("Text not found!", actions.checkLabel().contains(label));
     }
 }
